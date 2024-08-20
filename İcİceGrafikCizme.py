7476c992987a49617dbd96d2a355d2096c3b8a0d
emirkacar <emirkacar86@gmail.com>
2024-08-20 19:05:42 +0300

    Add files via upload

---
+++ İcİceGrafikCizme.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(1,6)
+
+y = np.arange(2,11,2)
+
+
+
+
+fig = plt.figure()
+
+
+axes1 = fig.add_axes([0.1,0.3,0.8,0.6])
+axes2 = fig.add_axes([0.2,0.5,0.1,0.3])
+axes1.plot(x,y)
+axes1.set_xlabel("Outer X")
+axes1.set_ylabel("Outer Y")
+axes1.set_title("Outer Graph")
+
+axes2.plot(y,x)
+axes2.set_xlabel("Inner X")
+axes2.set_ylabel("Inner Y")
+axes2.set_title("Outer Graph")
+
+
+
+print(plt.show())
+
+
+
+
+
